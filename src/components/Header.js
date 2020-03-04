94140c79672b1a75c7e34db7f5662ffd76107dbf
Andry <andry03071993@gmail.com>
2020-03-04 16:35:45 +0200

    add student

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ function Header(props) {
         <nav className='nav-link bg-secondary'>
             <span className="navbar-brand mb-0 h1">It school</span>
             <Link to={`/`}>
-                <button className='btn btn-success'>Home</button>
+                <button className='btn btn-info'>Home</button>
             </Link>
             <Link to={`/groups`}>
-                <button className='btn btn-success'>Groups</button>
+                <button className='btn btn-info'>Groups</button>
             </Link>
             <Link to={`/students`}>
-                <button className='btn btn-success'>Students</button>
+                <button className='btn btn-info'>Students</button>
             </Link>
         </nav>
     );
